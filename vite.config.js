aa1fc4ec81d4301ada27b677afe7e8d5060a4baa
Yazan <abo.ayash.yazan@gmail.com>
2024-11-19 15:27:24 +0100

    switch Server to Bind to all available interfaces

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   //base: '/coldbydefault.github.io/',
   server: {
-    host: '0.0.0.0', // Bind to all available interfaces
-    port: 5173,      // Optional: specify a custom port
+    host: '0.0.0.0', 
+    port: 5173,      
   },
 });
