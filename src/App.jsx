56c4c741fb50bcc723b4e4a493b388a593da3eec
Yazan <abo.ayash.yazan@gmail.com>
2024-11-19 11:49:22 +0100

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,20 @@
 
 import React from 'react';
 import Landing from './components/Landing';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
+import Navbar from './components/Navbar';
 import Intro from './components/Intro';
 import Portfolio from './components/Portfolio';
-import Timeline from './components/Timeline';
+
+
 
 
 function App() {
   return (
     <div>
-      {/* <Landing /> */} 
+      <Navbar />
       <Landing />
       <Intro />
       <Portfolio />
-      <Timeline />
-      <Contact />
-      <Footer />
     </div>
     
   );
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,104 @@
+import React, { useState } from "react";
+
+function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <>
+      <header className="relative flex flex-wrap sm:justify-start sm:flex-nowrap w-full bg-white text-sm py-3">
+        <nav className="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between">
+          <div className="flex items-center justify-between">
+            <a
+              className="flex-none text-xl font-semibold focus:outline-none focus:opacity-80"
+              href="#"
+              aria-label="Brand"
+            >ColdByDefault
+            </a>
+            <div className="sm:hidden">
+              <button
+                type="button"
+                onClick={handleMenuToggle}
+                className="relative size-7 flex justify-center items-center gap-x-2 rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none"
+                aria-expanded={isMenuOpen}
+                aria-controls="navbar-menu"
+                aria-label="Toggle navigation"
+              >
+                {isMenuOpen ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <path d="M18 6 6 18" />
+                    <path d="m6 6 12 12" />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <line x1="3" x2="21" y1="6" y2="6" />
+                    <line x1="3" x2="21" y1="12" y2="12" />
+                    <line x1="3" x2="21" y1="18" y2="18" />
+                  </svg>
+                )}
+                <span className="sr-only">Toggle navigation</span>
+              </button>
+            </div>
+          </div>
+          <div
+            id="navbar-menu"
+            className={`${
+              isMenuOpen ? "block" : "hidden"
+            } sm:block transition-all duration-300`}
+          >
+            <div className="flex flex-col gap-5 mt-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:ps-5">
+              <a
+                className="font-medium text-blue-500 focus:outline-none"
+                href="#"
+                aria-current="page"
+              >
+                Home
+              </a>
+              <a
+                className="font-medium text-gray-600 hover:text-gray-400 focus:outline-none focus:text-gray-400"
+                href="#"
+              >
+                Projects
+              </a>
+              <a
+                className="font-medium text-gray-600 hover:text-gray-400 focus:outline-none focus:text-gray-400"
+                href="#"
+              >
+                Links
+              </a>
+              <a
+                className="font-medium text-gray-600 hover:text-gray-400 focus:outline-none focus:text-gray-400"
+                href="#"
+              >
+                Contact
+              </a>
+            </div>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
+}
+
+export default Navbar;
