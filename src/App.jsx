3fa060b391034c0a46157bd3f67a2f927800305c
Yazan <abo.ayash.yazan@gmail.com>
2024-11-21 17:35:44 +0100

    Impressum's taken down to Footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,16 @@
 
 // Everything you know; is a lie.
 
-import React, { useState } from 'react';
-import Navbar from './components/Navbar';
-import Intro from './components/Intro';
-import Portfolio from './components/Portfolio';
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import Intro from "./components/Intro";
+import Portfolio from "./components/Portfolio";
 import GradientBackground from "./components/GradientBackground";
-import Footer from './components/Footer';
+import Footer from "./components/Footer";
 import Impressum from "./components/Impressum";
 
 function App() {
-  const [activeSection, setActiveSection] = useState('Home');
+  const [activeSection, setActiveSection] = useState("Home");
 
   return (
     <div className="relative min-h-screen">
@@ -23,18 +23,17 @@ function App() {
       <div className="relative z-10">
         {/* Navbar */}
         <Navbar setActiveSection={setActiveSection} />
-        {activeSection === 'Home' && <Intro />}
-        {activeSection === 'Projects' && <Portfolio />}
-        {activeSection === 'Links' && <div>Links Section Coming Soon...</div>}
-        {activeSection === 'Contact' && <div>Contact Section Coming Soon...</div>}
-        {activeSection === 'Impressum' && <Impressum />}
+        {/* Render Content Dynamically Based on Active Section */}
+        {activeSection === "Home" && <Intro />}
+        {activeSection === "Projects" && <Portfolio />}
+        {activeSection === "Links" && <div>Links Section Coming Soon...</div>}
+        {activeSection === "Contact" && <div>Contact Section Coming Soon...</div>}
         {/* Footer */}
-        <Footer />
+        {activeSection === "Impressum" && <Impressum />}
+        <Footer setActiveSection={setActiveSection}/> 
       </div>
     </div>
   );
 }
 
 export default App;
-
-
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 
-function Footer() {
+function Footer({ setActiveSection }) {
   return (
     <footer className="bg-dark-blue text-white py-2 text-center">
-      <div className="container mx-auto">
-        <p className="text-sm">
+      <div className="container mx-auto flex items-center justify-around">
+        <p className="text-sm mb-2">
           Copyright © 2024{" "}
           <span className="font-bold text-main-Cyne">ColdByDefault</span>. All rights reserved.
         </p>
+        <button
+          className="font-medium cursor-pointer text-gray-600 hover:text-gray-400 
+          focus:outline-none focus:text-gray-400"
+          onClick={() => setActiveSection("Impressum")}>
+          Impressum
+        </button>
       </div>
     </footer>
   );
