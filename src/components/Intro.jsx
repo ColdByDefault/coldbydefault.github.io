5a66d828b674262fa623b3743843721043901381
Yazan <abo.ayash.yazan@gmail.com>
2024-11-22 13:29:20 +0100

    Intro fixed, sm & lg => + Timeline, and Skills from Intro.jsx not root

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -5,15 +5,20 @@ import Skills from "./Skills";
 function Intro() {
   return (
     <>
-      <div className="flex items-center justify-evenly min-h-screen cursor-default
-      flex-col text-center pt-20 pb-6 text-white">
-        <div>
-          <div className="flex flex-col-reverse md:flex-row items-center 
-          md:items-start md:justify-between">
-            <div className="md:pr-6 text-center md:text-left">
-              <h1 className="text-2xl md:text-6xl mb-1 md:mb-3 font-bold pt-8">
-                Yazan<br />Abo-Ayash
-              </h1>
+    <div className="flex items-center justify-evenly min-h-screen cursor-default
+    flex-col text-center pt-20 pb-6 text-white">
+        <div className="flex flex-col md:flex-row items-center md:justify-between">
+          <div className="mb-6 md:mb-0">
+            <img
+              src="/assets/me5.jpg"
+              alt="Yazan"
+              className="w-[250px] h-[250px] md:w-[300px] md:h-[300px] rounded-full object-cover mx-auto"/>
+          </div>
+          <div className="md:pl-6 text-center md:text-left flex-1 md:pt-12">
+            <h1 className="text-2xl md:text-6xl mb-1 md:mb-3 font-bold">
+              Yazan Abo-Ayash
+            </h1>
+            <div className="px-4 md:px-0">
               <p className="text-sm max-w-xl mb-6 font-bold">
                 Hi, I am currently learning to become a software developer, focusing
                 on Python and Machine Learning, with a passion for building and
@@ -28,25 +33,18 @@ function Intro() {
                 </a>.
               </p>
             </div>
-            <div className="mb-6 md:mb-0">
-              <img
-              src="/assets/me5.jpg"
-              alt="Yazan"
-              className="w-[250px] h-[250px] md:w-[300px] md:h-[300px] rounded-full object-cover mx-auto"/>
-            </div>
           </div>
         </div>
-        <div className="flex w-full justify-evenly flex-col md:flex-row
-            mt-32">
-                <div>
-                    <h2 className="text-3xl font-bold mb-6 text-gray-400">My Journey</h2>
-                    <Timeline />
-                </div>
-                <div>
-                    <h2 className="text-3xl font-bold mb-6 text-gray-400">My Skills</h2>
-                    <Skills />
-                </div>
-            </div>
+        <div className="flex w-full justify-evenly flex-col md:flex-row mt-32">
+          <div className="px-4 sm:px-0">
+            <h2 className="text-3xl font-bold mb-6 text-gray-400 sm:pl-16">My Journey</h2>
+            <Timeline />
+          </div>
+          <div>
+              <h2 className="text-3xl font-bold mb-6 text-gray-400">My Skills</h2>
+              <Skills />
+          </div>
+        </div>
       </div>
     </>
   );
