5a621b363c6bb2b6f953e03b341e810682b9c0b1
Yazan <abo.ayash.yazan@gmail.com>
2024-11-25 11:28:03 +0100

    last depoly for next 20 commits

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -8,11 +8,11 @@ import HeroSection from "./HeroSection";
 
 function Intro() {
   return (
-    <div className="min-h-screen flex flex-col items-center text-white px-4">
+    <div className="min-h-screen flex flex-col items-center text-white px-4 mt-20">
       {/* Profile Section */}
       <HeroSection />
       {/* Certifications and Timeline */}
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 w-full max-w-6xl mt-10">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 w-full max-w-6xl mt-20">
         {/* Left Column */}
         <div className="col-span-1">
           <Certifications />
@@ -24,7 +24,7 @@ function Intro() {
         </div>
       </div>
       {/* Skills */}
-      <div className="w-full max-w-6xl mt-12">
+      <div className="w-full max-w-6xl mt-20">
         <Skills />
       </div>
       {/* Call to Action */}
